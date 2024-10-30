90fe386e48cc1f0a95c0c08fa0af65bec9c9bcd1
dushimechri <d.christoph@alustudent.com>
2024-10-30 10:41:43 -0700

    task done

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     try:
         for i in range(x):
-            print("{}".format(my_list[i]), end=' ')
+            print(my_list[i], end=' ')
             count += 1
     except IndexError:
         pass 
@@ -11,6 +11,4 @@ def safe_print_list(my_list=[], x=0):
         print("put x as number")
     except NameError:
         print("try again")
-
-    finally:
-        print("nb_print:{}".format(count))
+    print(x)    
