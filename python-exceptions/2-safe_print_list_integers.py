2e78fc5eb93b5487034dd526f52b2ccf4468f8c3
dushimechri <d.christoph@alustudent.com>
2024-11-03 15:21:32 -0800

    task done

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    try:
+        for i in range(x):
+            if isinstance(my_list[i],int):
+                print("{:d}".format(my_list[i]), end='')
+                count += 1
+            else:
+                continue
+    except IndexError:
+        return "index too large"
+    return count
