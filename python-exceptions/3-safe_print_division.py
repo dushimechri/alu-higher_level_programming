8f2e88d02afb5ecf72c1254d7d3cedc56094aa9e
dushimechri <d.christoph@alustudent.com>
2024-11-05 14:20:51 -0800

    task done

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -6,4 +6,4 @@ def safe_print_division(a, b):
         result = "None"
     finally:
         print(f"Inside result: {result}")
-        print("{:d} / {:d} = {}".format(a, b, result))
+    return result
