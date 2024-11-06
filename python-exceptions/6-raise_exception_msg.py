c83613f214faab0fed7ee59c19a819c12ff041ff
dushimechri <d.christoph@alustudent.com>
2024-11-06 04:09:39 -0800

    task done

--- python-exceptions/6-raise_exception_msg.py
+++ python-exceptions/6-raise_exception_msg.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def raise_exception_msg(message=""):
-     raise Exception(message)
+    try:
+        raise NameError(message)
+    except NameError:
+        print(message)
 
 raise_exception_msg("C is fun")
