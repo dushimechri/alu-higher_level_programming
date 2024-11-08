3968ec9456a2cff43f09f6c13a80f7dcf4627359
dushimechri <d.christoph@alustudent.com>
2024-11-08 14:09:21 -0800

    task done

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     result = []
-    try:
-        for i in range(list_length):
-            results = my_list_1[i] / my_list_2[i]
-            result.append(results)
+    for i in range(list_length):
+        try:
+              # Check if both lists have elements at index i
+            if i >= len(my_list_1) or i >= len(my_list_2):
+                print("out of range")
+                result.append(0)
+            else:
+                results = my_list_1[i] / my_list_2[i]
+                result.append(results)
     except ZeroDivisionError:
         print("division by 0")
         result.append(0)
