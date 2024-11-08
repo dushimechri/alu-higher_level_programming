94c1c6e4a7cfb3b574286317c707441f03a88298
dushimechri <d.christoph@alustudent.com>
2024-11-08 13:07:07 -0800

    task done

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -3,7 +3,8 @@ def list_division(my_list_1, my_list_2, list_length):
     result = []
     try:
         for i in range(list_length):
-            result.append(my_list_1[i] / my_list_2[i])
+            results = my_list_1[i] / my_list_2[i]
+            result.append(results)
     except ZeroDivisionError:
         print("division by 0")
         result.append(0)
@@ -14,4 +15,4 @@ def list_division(my_list_1, my_list_2, list_length):
         print("out of range")
         result.append(0)
     finally:
-        return result
+        print(result)
