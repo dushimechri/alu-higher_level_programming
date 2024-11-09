9c295268ced372146cb4fdea4460381374ef22ce
dushimechri <d.christoph@alustudent.com>
2024-11-09 00:16:04 -0800

    task done

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -12,6 +12,7 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size  # Private instance attribute
+
     def area(self):
         """Public instance method that returns the area of the square."""
-        return self.__size ** 2 # Area = size squared
+        return self.__size ** 2   # Area = size squared
