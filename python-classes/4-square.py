f851cc9e9038073dcb58fc6e057ca61024da67d5
dushimechri <d.christoph@alustudent.com>
2024-11-09 00:38:03 -0800

    task done

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -11,17 +11,18 @@ class Square:
 
     @property
     def size(self):
+        """Getter for the size property."""
         return self.__size
 
     @size.setter
     def size(self, value):
-             """Instantiation with optional size."""
+        """Setter for the size property."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
-            raise ValueError("size must be >=0")
+            raise ValueError("size must be >= 0")
         self.__size = value  # Private instance attribute
 
     def area(self):
         """Public instance method that returns the area of the square."""
-        return self.__size ** 2   # Area = size squared
+        return self.__size ** 2  # Area = size squared
