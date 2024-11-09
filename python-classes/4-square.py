527cc7d8dda1d02171ef353f21dbb6f84d435618
dushimechri <d.christoph@alustudent.com>
2024-11-09 00:34:01 -0800

    task done

---
+++ python-classes/4-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Module that defines a square."""
+
+
+class Square:
+    """A class that defines a square by its size."""
+
+    def __init__(self, size=0):
+        self.size = size  # Use the setter to validate the initial size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+             """Instantiation with optional size."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >=0")
+        self.__size = value  # Private instance attribute
+
+    def area(self):
+        """Public instance method that returns the area of the square."""
+        return self.__size ** 2   # Area = size squared
