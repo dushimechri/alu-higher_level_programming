0f42a55c8d1b67a4e643a5efb72211398fe2558a
dushimechri <d.christoph@alustudent.com>
2024-11-08 23:58:10 -0800

    task done

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -6,9 +6,8 @@ class Square:
     """A class that defines a square by its size."""
     def __init__(self, size=0):
         """Instantiation with size."""
-        if not isinstance(size,int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size# Private instance attribute
-
+        self.__size = size  # Private instance attribute
