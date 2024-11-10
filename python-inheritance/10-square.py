463a6dd00d7c8c914f5dc5dd4f0bca770f9121d2
dushimechri <d.christoph@alustudent.com>
2024-11-10 12:38:02 -0800

    task done

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,43 +1,32 @@
 #!/usr/bin/python3
 """
-This module defines a Rectangle class that inherits from BaseGeometry.
+This module defines a Square class that inherits from Rectangle.
 """
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
 
 
-class Rectangle(BaseGeometry):
+class Square(Rectangle):
     """
-    Represents a rectangle, inheriting from
-    BaseGeometry for validation and area calculation.
+    Represents a square, which is a special type of\
+            rectangle with equal width and height.
     """
 
-    def __init__(self, width, height):
+    def __init__(self, size):
         """
-        Initializes a new Rectangle instance with validated width and height.
+        Initializes a new Square instance with validated size.
 
-        :param width: The width of the rectangle; must be a positive integer
-        :param height: The height of the rectangle; must be a positive integer
-        :raises TypeError: If width or height is not an integer
-        :raises ValueError: If width or height is not a positive integer
+        :param size: The size of the square sides; must be a positive integer
+        :raises TypeError: If size is not an integer
+        :raises ValueError: If size is not a positive integer
         """
-        self.integer_validator("width", width)
-        self.__width = width
-        self.integer_validator("height", height)
-        self.__height = height
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
 
     def area(self):
         """
-        Calculates and returns the area of the rectangle.
-        :return: The area of the rectangle (width * height)
+        Calculates and returns the area of the square.
+        :return: The area of the square (size * size)
         """
-        return self.__width * self.__height
-
-    def __str__(self):
-        """
-        Returns a string representation of the rectangle in
-        the format: [Rectangle] <width>/<height>.
-
-        :return: String description of the rectangle
-        """
-        return f"[Rectangle] {self.__width}/{self.__height}"
+        return self.__size ** 2
