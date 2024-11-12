1824d5541639431545fdad5d9e0f0c6995caad48
dushimechri <d.christoph@alustudent.com>
2024-11-12 10:22:01 -0800

    task done

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""writing a function that reads a file"""
+"""writing a function that reads a file in which i will be using open command to open a file an also with statement"""
 
 
 def read_file(filename=""):
 """now let's pass the code down below"""
-
 
     with open(filename, 'r',encording='UTF-8') as f:
         return f.read()
