921be20ad6e4fdd64f7308f4db1584693efec16e
dushimechri <d.christoph@alustudent.com>
2024-11-13 02:51:47 -0800

    task done

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -17,4 +17,4 @@ def write_file(filename="", text=""):
         int: The number of characters written.
     """
     with open(filename, 'w', encoding='utf-8') as f:
-       return f.write(text)
+        return f.write(text)
