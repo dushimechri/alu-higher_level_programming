9f0e7cc53afb69ea96f7249e0380cd3668783aa2
dushimechri <d.christoph@alustudent.com>
2024-11-13 06:58:40 -0800

    task done

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -7,6 +7,6 @@ for JSON serialization of an object.
 
 
 def class_to_json(obj):
-     """Returns the dictionary description with simple data\
-            structure for JSON serialization of an object."""
-        return obj.__dict__
+    """Returns the dictionary description with simple data\
+         structure for JSON serialization of an object."""
+    return obj.__dict__
