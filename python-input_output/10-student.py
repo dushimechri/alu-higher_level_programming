404d3551bd38db4fa6a314f0aa2f5e553a0e8579
dushimechri <d.christoph@alustudent.com>
2024-11-14 13:24:02 -0800

    task done

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -43,5 +43,8 @@ class Student:
         if attrs is None:
             return self.__dict__
         else:
-            return {key: value for key, value in\
-                    self.__dict__.items() if key in attrs}
+            return {
+                    key: value
+                    for key, value in self.__dict__.items()
+                    if key in attrs
+                    }
