b7d9f150986b47f0d2dabbdf62864c6fd6d952d1
dushimechri <d.christoph@alustudent.com>
2024-11-14 13:20:17 -0800

    task done

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -8,7 +8,6 @@ This module deal with classes to ensure Student to JSON\
 class Student:
     """Class to define a student with first_name, last_name, and age."""
 
-
     def __init__(self, first_name, last_name, age):
         """
         Initialize the student with first_name, last_name, and age.
@@ -44,4 +43,5 @@ class Student:
         if attrs is None:
             return self.__dict__
         else:
-            return {key: value for key, value in self.__dict__.items() if key in attrs}
+            return {key: value for key, value in\
+                    self.__dict__.items() if key in attrs}
