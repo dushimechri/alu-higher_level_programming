88c276cb2567a62060ac85ae16805b635937df79
dushimechri <d.christoph@alustudent.com>
2024-11-16 12:51:16 -0800

    task done

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,7 +1,8 @@
--- a script that creates the MySQL server user user_0d_1 and grant all priviledges
-CREATE USER
-    IF NOT EXISTS 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES
-   ON *.*
-   TO 'user_0d_1'@'localhost';
+-- Check if user exists, and only create if not.
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to the user
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- Reload privileges to ensure changes take effect
+FLUSH PRIVILEGES;
