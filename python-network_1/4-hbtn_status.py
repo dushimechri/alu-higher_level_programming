2c8ac69a7960fa414d36dd0e9da9b568d10b7537
dushimechri <d.christoph@alustudent.com>
2024-12-01 19:29:30 +0000

    task done

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status."""
+"""
+Python script that fetches an URL with requests package
+"""
 import requests
 
+
 if __name__ == "__main__":
-    # Send GET request
-    r = requests.get("https://alu-intranet.hbtn.io/status")
-    
-    # Check if the request was successful (status code 200)
-    if r.status_code == 200:
-        print("Body response:")
-        print("\t- type: {}".format(type(r.text)))  # Type of the response body (string)
-        print("\t- content: {}".format(r.text))    # The content of the response (HTML as text)
-    else:
-        print("Request failed with status code:", r.status_code)
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    t = r.text
+    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))
