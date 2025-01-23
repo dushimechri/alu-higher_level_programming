9b2ab86bba50a87b250c33180d6d79638375cbe9
root <root@DESKTOP-PHM3FC2.localdomain>
2025-01-23 17:30:33 +0200

    task done

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
 if (args.length === 1 || args.length === 0) {
-	  console.log(0);
+  console.log(0);
 } else {
-	  const sorted = args.map(Number).sort((a, b) => b - a);
-	  console.log(sorted[1]);
+  const sorted = args.map(Number).sort((a, b) => b - a);
+  console.log(sorted[1]);
 }
