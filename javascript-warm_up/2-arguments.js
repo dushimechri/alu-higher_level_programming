12b0921a254a8fb7c02badcfa2a32be8b23fb3b0
root <root@DESKTOP-PHM3FC2.localdomain>
2025-01-23 14:45:56 +0200

    task done

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const arguments = process.argv.slice(2);
+if (arguments.length === 0) {
+	console.log(`No argument`);
+}
+else if (arguments.length === 1) {
+	console.log(`Argument found`);
+}
+else {
+	console.log(`Argument found`);
+}
