9cc0853ad92cd73edf9d61cd200ffda3fafd086f
root <root@DESKTOP-PHM3FC2.localdomain>
2025-01-26 19:25:37 +0200

    task done

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 if (Number.isInteger(parseInt(process.argv[2]))) {
   console.log(`My number: ${parseInt(process.argv[2])}`);
-}
-else{
+}else {
   console.log('Not a number');
-}
+};
--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const MyArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let i of  MyArray) {
-	console.log(i);
-}
+for (const i of  MyArray) {
+  console.log(i);
+};
