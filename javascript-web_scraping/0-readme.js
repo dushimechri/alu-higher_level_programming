6e29a3385317146b10c837b574622dd9fe5a940d
root <root@DESKTOP-PHM3FC2.localdomain>
2025-02-07 21:05:20 +0200

    task done

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 const fs = require('fs');
 const argvs = argv[0];
-const erroe_Object = { Error: ENOENT: no such file or directory, open 'doesntexist'
-	    at Error (native)
-	  errno: -2,
-	  code: 'ENOENT',
-	  syscall: 'open',
-	  path: 'doesntexist' }
+const erroe_Object = {code: 'ENOENT',
+  message: 'no such file or directory, open \'doesntexist\'',
+  errno: -2,
+  syscall: 'open',
+  path: 'doesntexist'}
 fs.open('argvs', 'utf-8', (err, data) => {
   if (err){
     console.log(erroe_Object);
