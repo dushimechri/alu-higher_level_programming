b7e66b9d104ed1e60c964a8b467de13da27daa6a
root <root@DESKTOP-PHM3FC2.localdomain>
2025-02-07 22:57:53 +0200

    task done

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const path= process.argv[2];
 const string = process.argv[3];
-fs.writeFile(path, string , 'utf-8',(err) {
+fs.writeFile(path, string, 'utf-8', (err) => {
   if (err) {
 	  console.log(err);
   }
