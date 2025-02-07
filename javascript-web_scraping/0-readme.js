f7c2da339bd78208fe69684cae5ec450ab55490a
root <root@DESKTOP-PHM3FC2.localdomain>
2025-02-07 21:14:25 +0200

    task done

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,14 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
 const argvs = process.argv[2];
-const erroe_Object = {code: 'ENOENT',
-  message: 'no such file or directory, open \'doesntexist\'',
-  errno: -2,
-  syscall: 'open',
-  path: 'doesntexist'}
-fs.open('argvs', 'r', (err, data) => {
+fs.readFile(argvs, 'utf-8', (err, data) => {
   if (err){
-    console.log(erroe_Object);
+    console.log(err);
   }
   console.log(data)
   
